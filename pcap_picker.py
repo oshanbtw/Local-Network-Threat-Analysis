e3b87213932cf63f121124a1f965db3e9a05827e
Oguzhan ERKAHRAMAN <oguzhanerkhrmn@gmail.com>
2023-02-17 22:17:31 +0300

    add try-except and logging

--- pcap_picker.py
+++ pcap_picker.py
@@ -1,14 +1,24 @@
 import subprocess
 import time
 import os
+import logging
+
+logging.basicConfig(filename='picker.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
 
 def pick():
-    subprocess.Popen(["sudo","tcpdump", "-w", "/home/erkahraman/Belgeler/python_dosyalari/NFStreamTest/traffic.pcap"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    time.sleep(598)
+    try:
+        logging.info("Tcpdump Start")
+        subprocess.Popen(["sudo","tcpdump", "-w", "/home/erkahraman/Belgeler/python_dosyalari/NFStreamTest/traffic.pcap"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        time.sleep(598)
 
-    # tcpdump'u durdurur
-    subprocess.Popen(["sudo","pkill", "tcpdump"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    os.system("mv /home/erkahraman/Belgeler/python_dosyalari/NFStreamTest/traffic.pcap /home/erkahraman/Belgeler/python_dosyalari/NFStreamTest/pcaps/")
+        # tcpdump'u durdurur
+        subprocess.Popen(["sudo","pkill", "tcpdump"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        logging.info("Tcpdump Stop")
+        logging.info("Move Start")
+        os.system("mv /home/erkahraman/Belgeler/python_dosyalari/NFStreamTest/traffic.pcap /home/erkahraman/Belgeler/python_dosyalari/NFStreamTest/pcaps/")
+        logging.info("Succsess!\n")
+    except Exception as e:
+        logging.error(str(e) + "\n")
 
 
 pick()
--- pcap_pusher.py
+++ pcap_pusher.py
@@ -3,45 +3,61 @@ from pymongo import MongoClient
 from datetime import datetime
 import pandas as pd
 import os
+import logging
+
+logging.basicConfig(filename='pusher.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
 
 
 def pcap_reader(pcap_path):
-    df = NFStreamer(source=pcap_path,
-                         decode_tunnels=True,
-                         bpf_filter=None,
-                         promiscuous_mode=True,
-                         snapshot_length=1536,
-                         idle_timeout=120,
-                         active_timeout=1800,
-                         accounting_mode=0,
-                         udps=None,
-                         n_dissections=20,
-                         statistical_analysis=True,
-                         splt_analysis=0,
-                         n_meters=0,
-                         max_nflows=0,
-                         performance_report=0,
-                         system_visibility_mode=0,
-                         system_visibility_poll_ms=100).to_pandas()
+    try:
+        logging.info("Pcap Reading")
+        df = NFStreamer(source=pcap_path,
+                            decode_tunnels=True,
+                            bpf_filter=None,
+                            promiscuous_mode=True,
+                            snapshot_length=1536,
+                            idle_timeout=120,
+                            active_timeout=1800,
+                            accounting_mode=0,
+                            udps=None,
+                            n_dissections=20,
+                            statistical_analysis=True,
+                            splt_analysis=0,
+                            n_meters=0,
+                            max_nflows=0,
+                            performance_report=0,
+                            system_visibility_mode=0,
+                            system_visibility_poll_ms=100).to_pandas()
 
+        logging.info("Time Changing")
+        #Timestamp olan değerleri datetime'a çevirip dataframe'a aktarıyorum.
+        df['bidirectional_first_seen'] = pd.to_datetime(df['bidirectional_first_seen_ms'], unit='ms')
+        df['bidirectional_last_seen'] = pd.to_datetime(df['bidirectional_last_seen_ms'], unit='ms')
 
-    #Timestamp olan değerleri datetime'a çevirip dataframe'a aktarıyorum.
-    df['bidirectional_first_seen'] = pd.to_datetime(df['bidirectional_first_seen_ms'], unit='ms')
-    df['bidirectional_last_seen'] = pd.to_datetime(df['bidirectional_last_seen_ms'], unit='ms')
+        logging.info("Pcap Removing")
+        #Dosyayı okuduktan sonra siliyorum.
+        os.remove(pcap_path)
+        logging.info("Succsess!")
 
-    #Dosyayı okuduktan sonra siliyorum.
-    os.remove(pcap_path)
-
-    return df
+        return df
+    except Exception as e:
+        logging.error("Error:",e)
 
 def database_connection(df):
-    #Database connection    
-    client = MongoClient("mongodb+srv://admin_oe:oe123456789@flows.lizea4s.mongodb.net/test")
-    db = client["Main"]
-    collection = db["flows"]
+    try:
+        logging.info("Connecting to database.")
+        #Database connection    
+        client = MongoClient("mongodb+srv://admin_oe:oe123456789@flows.lizea4s.mongodb.net/test")
+        db = client["Main"]
+        collection = db["flows"]
 
-    #Push to database.
-    collection.insert_many(df.to_dict('records'))
+        logging.info("Pushing to database.")
+        #Push to database.
+        collection.insert_many(df.to_dict('records'))
+        logging.info("Succsess!\n")
+
+    except Exception as e:
+        logging.error(str(e) + "\n")
 
 pcap_path = "/home/erkahraman/Belgeler/python_dosyalari/NFStreamTest/pcaps/traffic.pcap"
 data = pcap_reader(pcap_path)
