bae0d9558767f64abc9ae8f4ed5b7d84ac4815bc
Oguzhan ERKAHRAMAN <oguzhanerkhrmn@gmail.com>
2023-02-22 22:40:06 +0300

    Add logging files

--- Auto-Attackers/auto_receiver_icmp_exfiltration.py
+++ Auto-Attackers/auto_receiver_icmp_exfiltration.py
@@ -1,4 +1,13 @@
 import os
+import logging
+
+logging.basicConfig(filename='receiver.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
+
+counter = 1
 
 while True:
+    logging.info("Counter:", counter)
+    logging.info("Receiving")
     os.system("./qsreceive receive -l 192.168.1.49 -f received_file.txt")
+    logging.info("Received\n")
+    counter += 1
